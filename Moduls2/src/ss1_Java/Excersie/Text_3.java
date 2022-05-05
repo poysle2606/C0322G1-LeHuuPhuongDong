7b24774040d0992b145ae6c4e27a5f6aae4351f5
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-05-05 16:42:33 +0700

    Update

--- Moduls2/src/ss1_Java/Excersie/Text_3.java
+++ Moduls2/src/ss1_Java/Excersie/Text_3.java
@@ -1,4 +1,16 @@
 package ss1_Java.Excersie;
 
+import java.util.Scanner;
+
 public class Text_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double vnd = 23000;
+        double usd;
+        System.out.println("Enter USD to change: ");
+        usd = scanner.nextDouble();
+        vnd = usd * vnd;
+        System.out.println(vnd + " VND");
+
+    }
 }
