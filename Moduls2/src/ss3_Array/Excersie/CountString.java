5257bb0f5236968c0971b064faaae139470cc248
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-05-07 15:44:23 +0700

    update HomeWork

---
+++ Moduls2/src/ss3_Array/Excersie/CountString.java
@@ -0,0 +1,29 @@
+package ss3_Array.Excersie;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value of string:");
+        String str = scanner.nextLine();
+        char[] str1 = str.toCharArray();
+
+      //  System.out.println(Arrays.toString(str1));
+        //hàm to chararay chuyển chuỗi thành mảng;
+        
+        System.out.println("Enter word to find: ");
+        String word = scanner.nextLine();
+        char[] word1 = word.toCharArray();
+
+        int count = 0;
+        for (int i = 0; i < str1.length; i++) {
+            if(word1[0] == str1[i]) {
+                count++;
+            }
+
+        }
+        System.out.println(word + " xuat hien " + count + " lan");
+    }
+}
---
+++ Moduls2/src/ss3_Array/Excersie/TotalDuongCheo.java
@@ -0,0 +1,33 @@
+package ss3_Array.Excersie;
+
+import java.util.Scanner;
+
+public class TotalDuongCheo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter size at height of array:");
+        int size1 = input.nextInt();
+        System.out.println("Enter size at length of array:");
+        int size2 = input.nextInt();
+        int [][] numberArray = new int[size1][size2];
+        System.out.println("Enter " + numberArray.length + " height and " +
+                numberArray[0].length + " length: ");
+        for (int row = 0; row < numberArray.length; row++) {
+            for (int col = 0; col < numberArray[row].length; col++) {
+                numberArray[row][col] = input.nextInt();
+
+            }
+        }
+        int total = 0;
+        for (int i = 0;  i < numberArray.length; i++) {
+            for (int j = 0; j < numberArray[i].length; j++) {
+                if (numberArray[i] == numberArray[j]) {
+                    total += numberArray[i][j];
+                }
+
+            }
+
+        }
+        System.out.println(total);
+    }
+}
