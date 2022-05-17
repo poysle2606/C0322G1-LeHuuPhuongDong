4e0801fb68f2c70da45864057f59445345894859
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-05-17 08:01:47 +0700

    up

--- Moduls2/src/ss8_CleanCode/Excersie/TennisGame.java
+++ Moduls2/src/ss8_CleanCode/Excersie/TennisGame.java
@@ -23,35 +23,45 @@ public class TennisGame {
                     break;
 
             }
-        } else if (score1 >= 4 || score2 >= 4) {
-            int minusResult = score1 - score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = score1;
-                else {
-                    score.append("-");
-                    tempScore = score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
+            boolean text1 = score1 >= 4;
+            boolean text2 = score2 >= 4;
+            if (text1 || text2) {
+                score = getStringBuilder(score1, score2);
+            } else {
+                for (int i = 1; i < 3; i++) {
+                    if (i == 1) tempScore = score1;
+                    else {
+                        score.append("-");
+                        tempScore = score2;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            score.append("Love");
+                            break;
+                        case 1:
+                            score.append("Fifteen");
+                            break;
+                        case 2:
+                            score.append("Thirty");
+                            break;
+                        case 3:
+                            score.append("Forty");
+                            break;
+                    }
                 }
             }
         }
         return score.toString();
+    }
+
+    private static StringBuilder getStringBuilder(int score1, int score2) {
+        StringBuilder score;
+        int minusResult = score1 - score2;
+        if (minusResult == 1) score = new StringBuilder("Advantage player1");
+        else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+        else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
+        return score;
     }
 }
