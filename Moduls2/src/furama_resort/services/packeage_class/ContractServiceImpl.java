90003f7ddc7ba8772dd64941786be34ffca2f8a9
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-06-04 15:19:33 +0700

    up

--- Moduls2/src/furama_resort/services/packeage_class/ContractServiceImpl.java
+++ Moduls2/src/furama_resort/services/packeage_class/ContractServiceImpl.java
@@ -3,23 +3,51 @@ package furama_resort.services.packeage_class;
 import furama_resort.models.Booking;
 import furama_resort.models.Contract;
 import furama_resort.services.package_impl.ContactService;
+import furama_resort.services.utils.ReadAndWrite;
 
 import java.util.*;
 
 public class ContractServiceImpl implements ContactService {
     static List<Contract> contractList = new ArrayList<>();
+    private static final String LINK_CONTRACT = "D:\\C0322G1-LeHuuPhuongDong\\Moduls2\\src\\furama_resort\\files\\contract.csv";
 
     static Scanner input = new Scanner(System.in);
 
     @Override
     public void display() {
-        for (Contract list : contractList) {
-            System.out.println(list);
+
+        List<String[]> list = ReadAndWrite.readFile(LINK_CONTRACT);
+        contractList.clear();
+        for (String[] item : list) {
+            Contract contract = new Contract(Integer.parseInt(item[0]),
+                    Integer.parseInt(item[1]),
+                    Double.parseDouble(item[2]),
+                    Double.parseDouble(item[3]),
+                    Integer.parseInt(item[4]));
+            contractList.add(contract);
+        }
+        if (contractList.isEmpty()) {
+            System.err.println("This is list is empty.");
+        }
+
+        for (Contract list1 : contractList) {
+            System.out.println(list1);
         }
     }
 
     @Override
     public void add() {
+        List<String[]> list = ReadAndWrite.readFile(LINK_CONTRACT);
+
+        for (String[] item : list) {
+            Contract contract = new Contract(Integer.parseInt(item[0]),
+                    Integer.parseInt(item[1]),
+                    Double.parseDouble(item[2]),
+                    Double.parseDouble(item[3]),
+                    Integer.parseInt(item[4]));
+            contractList.add(contract);
+        }
+
         Queue<Booking> contracts = new LinkedList<>();
 
         Set<Booking> bookingSet = new BookingServiceImpl().sendBooking();
@@ -50,6 +78,17 @@ public class ContractServiceImpl implements ContactService {
             contractList.add(contract);
 
             System.out.println("Tạo hợp đồng thành công " + contract);
+
+            String line = "";
+            for (Contract item1 : contractList) {
+                line += String.format("%s,%s,%s,%s,%s",
+                        item1.getCodeContract(),
+                        item1.getCodeBooking(),
+                        item1.getFirstMoney(),
+                        item1.getTotalMoney(),
+                        item1.getIdCustomer()) + "\n";
+            }
+            ReadAndWrite.writeFile(LINK_CONTRACT, line);
         }
     }
 
